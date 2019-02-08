1f5e7ca5c1b5a0433401a8459be3a157e7f1854c
lkIcom2000 <47300409+lkIcom2000@users.noreply.github.com>
2019-02-08 22:01:52 +0100

    Add files via upload
    die aktuellstevVersion

---
+++ Münzspielo/Space.java
@@ -0,0 +1,59 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Space. The final frontier. 
+ * 
+ * @author Michael Kšlling
+ * @version 1.1
+ */
+public class Space extends World
+{
+    private int anzlCoins=60;
+    private int time=30;
+    ;
+    /**
+     * 
+     */
+    public Space()
+    {    
+        super(960, 620, 1);
+
+        createCoins();
+        //addObject(new Counter());
+    }
+
+    /**
+     */
+    public void createCoins()
+    {
+        for (int i=0;i<=anzlCoins;i++){
+            int x = Greenfoot.getRandomNumber(getWidth());
+            int y = Greenfoot.getRandomNumber(getHeight());
+            int values=3;
+            int z=Greenfoot.getRandomNumber(10);
+            if(z<=5){
+            values=1;
+            } else if(z<9){
+            values=2;
+            }
+            
+            
+            Coin c=new Coin(28,Color.CYAN, this, values);
+            addObject(c,x,y);
+            if(c.isTouching()){
+                removeObject(c);
+                i--;
+            }           
+        }
+    }
+    
+    
+    
+    public void act(){
+       
+    }
+
+    /**
+     */
+   
+}
