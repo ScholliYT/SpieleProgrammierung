1312723ca97ab6a3b658b6fbdee7d0ced11ec60f
Taubenfuzzi <47110455+Taubenfuzzi@users.noreply.github.com>
2019-03-25 13:46:07 +0100

    added Scanner
    |o|

--- projects/schollenspiel_maven/src/main/java/de/greenfootdevz/schollenspiel/Actor/Floe.java
+++ projects/schollenspiel_maven/src/main/java/de/greenfootdevz/schollenspiel/Actor/Floe.java
@@ -1,53 +1,68 @@
-package de.greenfootdevz.schollenspiel.Actor;
+import greenfoot.*;  
+import java.util.List;
+import java.util.*;
 
-import greenfoot.Actor;
-import greenfoot.Color;
-import greenfoot.GreenfootImage;
-
-import java.util.Random;
-
-public class Floe extends Actor {
+public class Floe extends Actor
+{
     private boolean passAble;
     private int size;
-
-    public Floe(boolean passAble) {
+    private boolean finish;
+    public Floe(boolean passAble)
+    {
         this.passAble = passAble;
         Random rnd = new Random();
         this.size = 80;
         this.colorSetup(Color.WHITE);
+        this.finish = false;
     }
-
-    public Floe(boolean passAble, int size) {
+    
+    public Floe(boolean passAble, int size)
+    {
         this.passAble = passAble;
         this.size = size;
         this.colorSetup(Color.WHITE);
+        this.finish = false;
     }
 
-    public void act() {
-        if (!this.passAble)
-            this.removeTouching(Player.class);
+    public void act() 
+    {
+        if(!this.passAble) this.removeTouching(Player.class);
+            
+        if(this.isTouching(Player.class) && this.finish)
+        {
+            ((IceWorld) this.getWorld()).ausgeben(((IceWorld) this.getWorld()).getWinners());
+        }
+    }   
+    
+    public void setFinish(boolean finish)
+    {
+        this.finish = finish;
     }
 
-    public void colorSetup(Color color) {
-        GreenfootImage image = new GreenfootImage(size - 10, size - 10);
+    public void colorSetup(Color color)
+    {
+        GreenfootImage image = new GreenfootImage(size-10, size-10);
         image.setColor(color);
-        image.fillOval(0, 0, size - 11, size - 11);
+        image.fillOval(0, 0, size-11, size-11);
         image.fill();
         this.setImage(image);
     }
-
-    public void paths(int x, int y, Color color) {
+    
+    public void paths(int x, int y, Color color)
+    {
         GreenfootImage image = new GreenfootImage(x, y);
         image.setColor(color);
         image.fill();
         this.setImage(image);
     }
-
-    public void setSate(boolean passAble) {
+    
+    public void setSate(boolean passAble)
+    {
         this.passAble = passAble;
     }
-
-    public boolean isTouching() {
+    
+    public boolean isTouching()
+    {
         return this.isTouching(Player.class);
     }
 }
---
+++ projects/schollenspiel_maven/src/main/java/de/greenfootdevz/schollenspiel/Actor/ScannerIceWorld.java
@@ -0,0 +1,33 @@
+import greenfoot.*;
+import java.util.*;
+public class ScannerIceWorld extends Actor
+{
+    public ScannerIceWorld()
+    {
+       
+    }
+
+    public void act()
+    {
+        List player = this.getWorld().getObjects(Player.class);
+        if(player.size() == 0)
+            this.stoppen();
+
+        int i = 0;    
+        for(Player p: this.getWorld().getObjects(Player.class))
+        {
+            if(!p.isMovable())
+                i++;
+        }
+
+        if((i == player.size()))
+            this.stoppen();
+    }
+
+    private void stoppen()
+    {
+       ((IceWorld) this.getWorld()).ausgeben(((IceWorld) this.getWorld()).getWinners());
+    }
+
+    
+}
